1b109fd16f1b8e6dc49412db07c0047bba40d9b0
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 18:10:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,6 +10,7 @@ end
 
 def take_a_number(katz_deli)
   line_size = []
+    while katz_deli > 0 
     katz_deli.each_with_index{|people, index| line_size << "#{index + 1}. #{people}"}
     puts "The line is currently: #{line_size.join('')}"
   
