66f30dc446cc477dc56da173fe5ee662cb353cd6
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 17:46:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+katz_deli = []
+
+def line 
+  
+  if katz_deli == 0
+  puts "The line is currently empty."
+  
+end
+
+def take_a_number(array, people)
+  if katz_deli > 1 
+    array.each 
