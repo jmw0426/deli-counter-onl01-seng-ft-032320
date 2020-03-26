3d411050ad246839d3ee27d68bcd5b0d156b0222
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 18:08:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,14 @@ katz_deli = []
 def line(katz_deli) 
   if katz_deli.length == 0
   puts "The line is currently empty."
-  
-  else 
-    line_size = []
-    katz_deli.each_with_index{|people, index| line_size << "#{index + 1}. #{people}"}
-    puts "The line is currently: #{line_size.join}"
   end
 end    
 
+def take_a_number(katz_deli)
+  line_size = []
+    katz_deli.each_with_index{|people, index| line_size << "#{index + 1}. #{people}"}
+    puts "The line is currently: #{line_size.join(''}"
+    
+  end
+end  
+  
